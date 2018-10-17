d11922d8a0957ef3885db5f284fe5e3f75635910
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2018-10-17 07:40:43 -0700

    .

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 application = Flask(__name__)
 
-@app.route("/")
+
+@application.route("/")
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
