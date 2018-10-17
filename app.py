335ea3321e3674b0ae8e7a24b6d313a9a9ef8fe8
Seth Rainsdon <Seth.Rainsdon@nunetnetworks.net>
2018-10-17 08:27:24 -0700

    ?

--- app.py
+++ app.py
@@ -7,5 +7,10 @@ def hello():
     return "Hello World!"
 
 
+@application.route("/test/")
+def testing():
+    return "Test is good"
+
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
